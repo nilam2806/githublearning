173d739e2bda0449b66cb874c2543d907ef1ee3d
Nilam <nilampatil8099@gmail.com>
2023-09-29 13:04:54 +0530

    Initial commit.....

---
+++ java_with_github/src/com/training/org/HelloGithub.java
@@ -0,0 +1,10 @@
+package com.training.org;
+
+public class HelloGithub {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("Welcome to java ");
+		System.out.println("hello");
+	}
+
+}
